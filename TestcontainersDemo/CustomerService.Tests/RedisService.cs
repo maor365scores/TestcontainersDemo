d422a97c479532dbc87e244a09432d66a886da85
Maor Rocky <maor@365scores.com>
2024-05-08 17:07:59 +0300

    redis

---
+++ TestcontainersDemo/CustomerService.Tests/RedisService.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace CustomerService.Tests;
+
+using StackExchange.Redis;
+
+public class RedisService
+{
+	private readonly ConnectionMultiplexer _redis;
+
+	public RedisService(string connectionString)
+	{
+		_redis = ConnectionMultiplexer.Connect(connectionString);
+	}
+
+	public void Set<T>(string key, T value)
+	{
+		var db = _redis.GetDatabase();
+		var serializedValue = JsonSerializer.Serialize(value);
+		db.StringSet(key, serializedValue);
+	}
+	
+	public T Get<T>(string key)
+	{
+		var db = _redis.GetDatabase();
+		var value = db.StringGet(key);
+		return JsonSerializer.Deserialize<T>(value);
+	}
+}
---
+++ TestcontainersDemo/CustomerService.Tests/RedisServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Testcontainers.Redis;
+
+namespace CustomerService.Tests;
+
+public class RedisServiceTests : IAsyncLifetime
+{
+	private readonly RedisContainer _redisContainer = new RedisBuilder().Build();
+
+	public Task InitializeAsync() => _redisContainer.StartAsync();
+
+	public Task DisposeAsync() => _redisContainer.DisposeAsync().AsTask();
+
+	[Fact]
+	public void SetAndGetValueFromRedis()
+	{
+		var redisService = new RedisService(_redisContainer.GetConnectionString());
+		const string key = "testKey";
+		const string value = "testValue";
+
+		redisService.Set(key, value);
+		var retrievedValue = redisService.Get<string>(key);
+
+		Assert.Equal(value, retrievedValue);
+	}
+
+	[Fact]
+	public void SetAndGetDateTimeFromRedis()
+	{
+		var redisService = new RedisService(_redisContainer.GetConnectionString());
+		const string key = "dateTimeKey";
+		var currentDateTime = DateTime.UtcNow;
+
+		redisService.Set(key, currentDateTime);
+		var retrievedDateTime = redisService.Get<DateTime>(key);
+
+		Assert.Equal(currentDateTime, retrievedDateTime);
+	}
+}
